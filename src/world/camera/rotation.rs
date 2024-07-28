8447bf7ceaecd96b632b37378fb50eb2c530dc5d
Stijn Vergauwen <stijnvergauwen@gmail.com>
2024-07-28 11:16:11 +0200

    add tests for camera rotation

--- src/world/camera/rotation.rs
+++ src/world/camera/rotation.rs
@@ -1,6 +1,6 @@
 use std::ops::Range;
 
-use bevy::{ecs::event::EventIterator, input::mouse::MouseMotion, prelude::*};
+use bevy::{input::mouse::MouseMotion, prelude::*};
 
 use crate::{utility::add_rotations_as_eulers, GameRunningSet};
 
@@ -8,7 +8,7 @@ use super::CameraAnchor;
 
 const ROTATION_MULTIPLIER: f32 = 0.0015;
 const ACTIVATE_ROTATION_INPUT_BUTTON: MouseButton = MouseButton::Right;
-const CAMERA_PITCH_RANGE_DEGREES: Range<f32> = 1.0..80.0;
+const CAMERA_PITCH_RANGE_DEGREES: Range<f32> = -80.0..-1.0;
 
 pub struct CameraRotationPlugin;
 
@@ -73,7 +73,7 @@ fn has_rotation_input(mouse_motion_is_empty: bool, mouse_input: &ButtonInput<Mou
     !mouse_motion_is_empty && mouse_input.pressed(ACTIVATE_ROTATION_INPUT_BUTTON)
 }
 
-fn sum_mouse_delta(motion_events: EventIterator<MouseMotion>) -> Vec2 {
+fn sum_mouse_delta<'a>(motion_events: impl Iterator<Item = &'a MouseMotion>) -> Vec2 {
     motion_events.fold(Vec2::ZERO, |sum, motion| sum + motion.delta)
 }
 
@@ -84,20 +84,33 @@ fn rotation_from_mouse_delta(mouse_delta: Vec2, rotation_multiplier: f32) -> Qua
 
 fn clamp_camera_pitch(camera_rotation: Quat, pitch_range_degrees: Range<f32>) -> Quat {
     let as_euler = camera_rotation.to_euler(EulerRot::YXZ);
-    let min = -pitch_range_degrees.start;
-    let max = -pitch_range_degrees.end;
+    let min = pitch_range_degrees.start.to_radians();
+    let max = pitch_range_degrees.end.to_radians();
 
-    Quat::from_euler(
-        EulerRot::YXZ,
-        as_euler.0,
-        as_euler.1.clamp(max.to_radians(), min.to_radians()),
-        as_euler.2,
-    )
+    let clamped_pitch = as_euler.1.clamp(min, max);
+
+    Quat::from_euler(EulerRot::YXZ, as_euler.0, clamped_pitch, as_euler.2).normalize()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn can_sum_mouse_delta() {
+        let motion_events = vec![
+            MouseMotion {
+                delta: Vec2::new(0.2, 1.0),
+            },
+            MouseMotion {
+                delta: Vec2::new(0.1, -0.4),
+            },
+        ];
+
+        let summed = sum_mouse_delta(motion_events.iter());
+
+        assert_eq!(Vec2::new(0.3, 0.6), summed);
+    }
 
     #[test]
     fn can_get_rotation_from_mouse_delta() {
@@ -113,5 +126,31 @@ mod tests {
         let as_rotation = rotation_from_mouse_delta(mouse_delta, rotation_multiplier);
 
         assert_eq!(expected, as_rotation);
+    }
+
+    #[test]
+    fn can_clamp_camera_pitch() {
+        let range = -60.0..-20.0;
+
+        let rotation_below_clamp = build_quat(-30.0, -80.0);
+        let rotation_in_clamp = build_quat(0.0, -50.0);
+        let rotation_above_clamp = build_quat(70.0, -10.0);
+
+        let clamped_below = clamp_camera_pitch(rotation_below_clamp, range.clone());
+        let clamped_in = clamp_camera_pitch(rotation_in_clamp, range.clone());
+        let clamped_above = clamp_camera_pitch(rotation_above_clamp, range.clone());
+
+        assert_eq!(0.0, build_quat(-30.0, -60.0).angle_between(clamped_below));
+        assert_eq!(0.0, build_quat(0.0, -50.0).angle_between(clamped_in));
+        assert_eq!(0.0, build_quat(70.0, -20.0).angle_between(clamped_above));
+
+        fn build_quat(yaw_deg: f32, pitch_deg: f32) -> Quat {
+            Quat::from_euler(
+                EulerRot::YXZ,
+                yaw_deg.to_radians(),
+                pitch_deg.to_radians(),
+                0.0,
+            )
+        }
     }
 }
