29c64972944e663db4cb7745f1691139ef9b3632
Stijn Vergauwen <stijnvergauwen@gmail.com>
2024-08-01 21:14:21 +0200

    refactor road builder

--- src/road/road_builder.rs
+++ src/road/road_builder.rs
@@ -2,7 +2,7 @@ use bevy::{color::palettes::tailwind::GRAY_600, prelude::*};
 
 use crate::{utility::mesh_builder::MeshBuilder, GameRunningSet};
 
-use super::{ActiveRoad, OnActiveRoadModified, RoadData, RoadEditor};
+use super::{ActiveRoad, OnActiveRoadModified, RoadData};
 
 const ROAD_LENGTH: f32 = 20.0;
 
@@ -33,41 +33,51 @@ impl RoadBuilder {
             warn!("build method called on a RoadBuilder that already contains mesh data.");
         }
 
-        // TODO: refactor
-
-        let road_width = road_data.total_width();
         let mut width_of_built_sections = 0.0;
 
         for component in road_data.components() {
-            let size = component.size();
-            let x_position = -road_width / 2.0 + width_of_built_sections + size.x / 2.0;
-            width_of_built_sections += size.x;
-
-            // Build left side quad
-            let transform = Transform::from_translation(Vec3::X * x_position)
-                .with_scale(Vec3::new(ROAD_LENGTH, size.y, size.x))
-                .looking_to(Vec3::NEG_X, Vec3::Y);
-
-            self.mesh_builder.add_quad(transform, Vec2::new(0.0, 0.0));
-
-            // Build top quad
-            let transform = Transform::from_translation(Vec3::X * x_position)
-                .with_scale(Vec3::new(ROAD_LENGTH, size.x, size.y))
-                .looking_to(Vec3::Y, Vec3::X);
-
-            self.mesh_builder.add_quad(transform, Vec2::new(0.0, 0.0));
-
-            // Build right side quad
-            let transform = Transform::from_translation(Vec3::X * x_position)
-                .with_scale(Vec3::new(ROAD_LENGTH, size.y, size.x))
-                .looking_to(Vec3::X, Vec3::Y);
-
-            self.mesh_builder.add_quad(transform, Vec2::new(0.0, 0.0));
+            self.build_road_component(
+                &mut width_of_built_sections,
+                component.size(),
+                road_data.total_width(),
+            )
         }
     }
 
     pub fn get_mesh(&self) -> Mesh {
         self.mesh_builder.to_mesh()
+    }
+
+    fn build_road_component(
+        &mut self,
+        width_of_built_sections: &mut f32,
+        component_size: Vec2,
+        road_width: f32,
+    ) {
+        let uv = Vec2::new(0.0, 0.0);
+        let x_position = calculate_x_position_of_road_component(
+            road_width,
+            *width_of_built_sections,
+            component_size,
+        );
+
+        // Build left side quad
+        self.mesh_builder.add_quad(
+            calculate_left_face_transform(x_position, component_size),
+            uv,
+        );
+
+        // Build top quad
+        self.mesh_builder
+            .add_quad(calculate_top_face_transform(x_position, component_size), uv);
+
+        // Build right side quad
+        self.mesh_builder.add_quad(
+            calculate_right_face_transform(x_position, component_size),
+            uv,
+        );
+
+        *width_of_built_sections += component_size.x;
     }
 }
 
@@ -75,15 +85,12 @@ fn redraw_road_on_modified(
     mut on_modified: EventReader<OnActiveRoadModified>,
     mut commands: Commands,
     mut active_road_query: Query<&mut Handle<Mesh>, With<ActiveRoad>>,
-    road_editor: Res<RoadEditor>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<StandardMaterial>>,
 ) {
-    for _ in on_modified.read() {
-        // TODO: refactor
-
+    for event in on_modified.read() {
         let mut road_builder = RoadBuilder::new();
-        road_builder.build_from_road_data(road_editor.road().clone());
+        road_builder.build_from_road_data(event.road().clone());
 
         let road_mesh = meshes.add(road_builder.get_mesh());
 
@@ -104,4 +111,32 @@ fn redraw_road_on_modified(
             ));
         }
     }
+}
+
+// Utils
+
+fn calculate_left_face_transform(x_position: f32, size: Vec2) -> Transform {
+    Transform::from_translation(Vec3::X * x_position)
+        .with_scale(Vec3::new(ROAD_LENGTH, size.y, size.x))
+        .looking_to(Vec3::NEG_X, Vec3::Y)
+}
+
+fn calculate_top_face_transform(x_position: f32, size: Vec2) -> Transform {
+    Transform::from_translation(Vec3::X * x_position)
+        .with_scale(Vec3::new(ROAD_LENGTH, size.x, size.y))
+        .looking_to(Vec3::Y, Vec3::X)
+}
+
+fn calculate_right_face_transform(x_position: f32, size: Vec2) -> Transform {
+    Transform::from_translation(Vec3::X * x_position)
+        .with_scale(Vec3::new(ROAD_LENGTH, size.y, size.x))
+        .looking_to(Vec3::X, Vec3::Y)
+}
+
+fn calculate_x_position_of_road_component(
+    road_width: f32,
+    width_of_built_sections: f32,
+    size: Vec2,
+) -> f32 {
+    -road_width / 2.0 + width_of_built_sections + size.x / 2.0
 }
