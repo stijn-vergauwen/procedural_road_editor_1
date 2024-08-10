3f93b388951f84eb4d84a69b1e6d43e9c079c067
Stijn Vergauwen <stijnvergauwen@gmail.com>
2024-08-10 18:44:23 +0200

    add color input display

--- src/ui/inputs/color_input.rs
+++ src/ui/inputs/color_input.rs
@@ -1,10 +1,9 @@
 use bevy::{color::palettes::tailwind::*, prelude::*};
 
-use crate::{utility::texture_builder::TextureBuilder, GameRunningSet};
+use crate::{ui::build_text_node, utility::texture_builder::TextureBuilder, GameRunningSet};
 
 use super::slider_input::{spawn_slider_input_with_image, OnSliderInputValueChanged};
 
-// TODO: update textures when color changes
 // TODO: display current color
 
 pub struct ColorInputPlugin;
@@ -17,7 +16,8 @@ impl Plugin for ColorInputPlugin {
                 Update,
                 (
                     send_color_input_changed_events.in_set(GameRunningSet::SendEvents),
-                    update_color_input_textures.in_set(GameRunningSet::UpdateEntities),
+                    (update_color_input_textures, update_color_input_display)
+                        .in_set(GameRunningSet::UpdateEntities),
                 ),
             );
     }
@@ -59,6 +59,17 @@ enum ColorChannel {
     Alpha,
 }
 
+#[derive(Component)]
+struct ColorInputDisplay {
+    color_input_entity: Entity,
+}
+
+impl ColorInputDisplay {
+    fn new(color_input_entity: Entity) -> Self {
+        Self { color_input_entity }
+    }
+}
+
 #[derive(Event)]
 struct OnColorInputValueChanged {
     color_input_entity: Entity,
@@ -85,7 +96,7 @@ fn spawn_test_thing(mut commands: Commands, mut images: ResMut<Assets<Image>>) {
             container
                 .spawn(build_background_section_node())
                 .with_children(|section| {
-                    spawn_color_input(section, &mut images, start_color);
+                    spawn_color_input(section, &mut images, start_color, Some("Color"));
                 });
         });
 }
@@ -94,31 +105,46 @@ pub fn spawn_color_input(
     builder: &mut ChildBuilder,
     images: &mut Assets<Image>,
     start_color: Color,
+    label: Option<impl Into<String>>,
 ) -> Entity {
     let mut color_input = builder.spawn(build_color_input_container_node());
     let color_input_entity = color_input.id();
 
     color_input.with_children(|color_input| {
+        if let Some(label) = label {
+            color_input.spawn(build_text_node(
+                label,
+                20.0,
+                Color::WHITE,
+                JustifyText::Center,
+                (),
+            ));
+        }
+
+        color_input.spawn(build_color_display_node(color_input_entity, start_color));
+
         spawn_color_input_slider(
             color_input,
             images,
             color_input_entity,
+            start_color,
             ColorChannel::A,
-            start_color,
         );
+
         spawn_color_input_slider(
             color_input,
             images,
             color_input_entity,
+            start_color,
             ColorChannel::B,
-            start_color,
         );
+
         spawn_color_input_slider(
             color_input,
             images,
             color_input_entity,
-            ColorChannel::C,
             start_color,
+            ColorChannel::C,
         );
     });
 
@@ -129,8 +155,8 @@ fn spawn_color_input_slider(
     builder: &mut ChildBuilder,
     images: &mut Assets<Image>,
     color_input_entity: Entity,
-    color_channel: ColorChannel,
     start_color: Color,
+    color_channel: ColorChannel,
 ) -> Entity {
     let image = images.add(generate_slider_image(start_color, color_channel));
 
@@ -191,6 +217,20 @@ fn update_color_input_textures(
     }
 }
 
+fn update_color_input_display(
+    mut on_color_changed: EventReader<OnColorInputValueChanged>,
+    mut color_input_display_query: Query<(&mut BackgroundColor, &ColorInputDisplay)>,
+) {
+    for event in on_color_changed.read() {
+        for (mut background_color, _) in color_input_display_query
+            .iter_mut()
+            .filter(|(_, display)| display.color_input_entity == event.color_input_entity)
+        {
+            *background_color = event.new_color.into();
+        }
+    }
+}
+
 // Utility
 
 fn generate_slider_image(color: Color, channel: ColorChannel) -> Image {
@@ -226,7 +266,9 @@ fn build_color_input_container_node() -> impl Bundle {
         NodeBundle {
             style: Style {
                 flex_direction: FlexDirection::Column,
-                row_gap: Val::Px(4.0),
+                align_items: AlignItems::Center,
+                row_gap: Val::Px(8.0),
+                padding: UiRect::all(Val::Px(4.0)),
                 ..default()
             },
             background_color: NEUTRAL_600.into(),
@@ -257,4 +299,21 @@ fn build_background_section_node() -> impl Bundle {
         background_color: NEUTRAL_700.into(),
         ..default()
     }
+}
+
+fn build_color_display_node(color_input_entity: Entity, start_color: Color) -> impl Bundle {
+    (
+        ColorInputDisplay::new(color_input_entity),
+        NodeBundle {
+            style: Style {
+                width: Val::Px(40.0),
+                height: Val::Px(40.0),
+                border: UiRect::all(Val::Px(4.0)),
+                ..default()
+            },
+            border_color: NEUTRAL_700.into(),
+            background_color: start_color.into(),
+            ..default()
+        },
+    )
 }
