b829f7986cb9e318aca6c74f96f4e8362e8ddb1a
Stijn Vergauwen <stijnvergauwen@gmail.com>
2024-10-12 09:07:08 +0200

    only init road drawer resources in road drawer mode

--- src/road_drawer.rs
+++ src/road_drawer.rs
@@ -7,6 +7,8 @@ use gizmos::RoadDrawerGizmosPlugin;
 use road_being_drawn::{RoadBeingDrawn, RoadBeingDrawnPlugin};
 use selected_road::SelectedRoadPlugin;
 
+use crate::game_modes::GameMode;
+
 pub struct RoadDrawerPlugin;
 
 impl Plugin for RoadDrawerPlugin {
@@ -16,12 +18,20 @@ impl Plugin for RoadDrawerPlugin {
             RoadDrawerGizmosPlugin,
             RoadBeingDrawnPlugin,
         ))
-        .init_resource::<RoadDrawer>();
+        .add_systems(OnEnter(GameMode::RoadDrawer), init_road_drawer)
+        .add_systems(OnExit(GameMode::RoadDrawer), remove_road_drawer);
     }
 }
 
-// TODO: only have resource exist in RoadDrawer mode
 #[derive(Resource, Default, Debug)]
 pub struct RoadDrawer {
     road_being_drawn: Option<RoadBeingDrawn>,
+}
+
+fn init_road_drawer(mut commands: Commands) {
+    commands.init_resource::<RoadDrawer>();
+}
+
+fn remove_road_drawer(mut commands: Commands) {
+    commands.remove_resource::<RoadDrawer>();
 }
--- src/road_drawer/selected_road.rs
+++ src/road_drawer/selected_road.rs
@@ -6,18 +6,18 @@ pub use events::OnSelectRoadRequested;
 use events::SelectedRoadEventsPlugin;
 use ui::SelectedRoadUiPlugin;
 
-use crate::road::road_data::RoadData;
+use crate::{game_modes::GameMode, road::road_data::RoadData};
 
 pub struct SelectedRoadPlugin;
 
 impl Plugin for SelectedRoadPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins((SelectedRoadUiPlugin, SelectedRoadEventsPlugin))
-            .insert_resource(SelectedRoad::default());
+            .add_systems(OnEnter(GameMode::RoadDrawer), init_selected_road)
+            .add_systems(OnExit(GameMode::RoadDrawer), remove_selected_road);
     }
 }
 
-// TODO: only have resource exist in RoadDrawer mode
 #[derive(Resource, Debug, Default)]
 pub struct SelectedRoad {
     selected_road: Option<RoadData>,
@@ -27,4 +27,12 @@ impl SelectedRoad {
     pub fn set_selected_road(&mut self, selected_road: RoadData) {
         self.selected_road = Some(selected_road);
     }
+}
+
+fn init_selected_road(mut commands: Commands) {
+    commands.init_resource::<SelectedRoad>();
+}
+
+fn remove_selected_road(mut commands: Commands) {
+    commands.remove_resource::<SelectedRoad>();
 }
